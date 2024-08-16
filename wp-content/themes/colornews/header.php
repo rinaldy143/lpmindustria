84c32adfbf9c1819bc93cf7f486ffb28f08b37f5
rinaldy143 <rinaldy143@gmail.com>
2024-08-16 11:06:18 +0700

    fix: slider main post

--- wp-content/themes/colornews/header.php
+++ wp-content/themes/colornews/header.php
@@ -15,8 +15,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	
 	<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/based.css">
 </head>
 
 <body <?php body_class(); ?>>
