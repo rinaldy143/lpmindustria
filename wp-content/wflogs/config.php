ff63590020807d6f24989b4623360cc6cf8303f1
rinaldy143 <rinaldy143@gmail.com>
2024-08-24 15:22:48 +0700

    fix: responsive mobile widget

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1724831967;s:7:"authKey";s:64:"YcLV/8>_mO?ZTeFr ?7F/(pw_@Im.<IF<%Ih,o,R/lzqS!S/q^9>o,jxbc)[:SEc";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:1860;}
