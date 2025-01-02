486318a35c7962e09f21f929fb0a14514615abb9
rinaldy143 <rinaldy143@gmail.com>
2025-01-02 16:11:07 +0700

    feat: add default img for widget one

--- wp-content/themes/colornews/inc/widgets/colornews-featured-post-style-one-widget.php
+++ wp-content/themes/colornews/inc/widgets/colornews-featured-post-style-one-widget.php
@@ -118,7 +118,7 @@ class colornews_featured_post_style_one_widget extends WP_Widget {
 								'alt'   => esc_attr( $image_alt_text ),
 							) ) . '</a>';
 					} else {
-						$big_image       = '<a href="' . get_permalink( $post->ID ) . '"><img src="' . get_template_directory_uri() . '/img/big-slider.png">' . '</a>';
+						$big_image       = '<a href="' . get_permalink( $post->ID ) . '"><img src="' . get_template_directory_uri() . '/img/Header-Politeknik-STMI.jpg">' . '</a>';
 						$thumbnail_image .= '<a data-slide-index="' . $j . '" href=""><img src="' . get_template_directory_uri() . '/img/big-slider-thumb.png">' . '</a>';
 					}
 					$big_image_output .= '<li>' . $big_image . '<div class="caption-wrapper"><div class="caption-desc"><h3 id="caption-title-one" class="caption-title"><a href="' . get_permalink() . '" title="' . esc_attr( $title_attribute ) . '">' . get_the_title() . '</a></h3><div class="caption-content">' . get_the_excerpt() . '</div></div><div class="slider-btn"><a href="' . get_permalink() . '">' . __( ' Read More <i class="fa fa-angle-double-right"></i>', 'colornews' ) . '</a></div></div><div class="slider-category">' . colornews_colored_category_return( 0 ) . '</div></li>';
